0bb28be2acb2b225e6077ad55e478691027fd2f1
Atemyn <atemyn@mail.ru>
2022-07-04 21:38:02 +0700

    Создан класс CurrencyController и обновлен файл application.properties

---
+++ src/main/java/com/practice/controllers/CurrencyController.java
@@ -0,0 +1,10 @@
+package com.practice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("currency")
+public class CurrencyController {
+
+}
